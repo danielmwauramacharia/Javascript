eb049dfa9cb4c40cb99c00bbe23c81a74b9712a6
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-01 12:13:40 +0300

    How to declare variables in javascript and also learning the concept of re-assigning of variables

---
+++ variable_declaration.js
@@ -0,0 +1,9 @@
+
+// // variable declared by const cannot be reassigned
+// const person = "John";
+// person = "Nick"; // Causes an error to occur
+
+
+let person = "Nick";
+person = "John"
+console.log(person) // reassigning is allowed with let
