3b8c6dec294b0923a287d2cdf1cece61de276356
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-01 12:35:34 +0300

    the var variable moved at the top of the scope during interpretetion- this is called Hoisting

---
+++ hoisting.js
@@ -0,0 +1,11 @@
+// var declared variables are moved at the top of scope during the execution
+
+
+console.log(myVar); // undefined - no error raised
+var myVar = 2;
+
+// this is how the above code is understood by the interpreter
+
+// var myVar1;
+// console.log(myVar1) // undefined - no error raised
+// myVar1 = 2; 
