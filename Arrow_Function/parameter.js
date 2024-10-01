5d0ea31e9d9d6dea7f23565a5f3b051aec71ea63
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-01 14:20:39 +0300

    how parentheses are applied based on the number of parameters

---
+++ Arrow_Function/parameter.js
@@ -0,0 +1,25 @@
+// if function has no parameter use empty parentheses ()
+//Example 1
+const greet = () => console.log("Hello");
+greet();
+
+//Example 2
+const myFunction = () => {
+    const x = 2;
+    return x;
+}
+
+console.log(myFunction());
+
+/*
+if only one parameter the parentheses can be avoided
+instead of const double1 = (x) => x * 2;
+*/
+const double1 = x => x * 2; 
+console.log(double1(2)); //4
+
+
+//To imlicitly return an object, parentheses need to be used
+const getPerson = () => ({name: "Nick", age: 24});
+console.log(getPerson());
+
